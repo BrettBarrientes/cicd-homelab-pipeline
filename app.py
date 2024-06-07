4f34e5575306f22567b6ea827e63380b0cffd2c1
Brett Barrientes <51255310+BrettBarrientes@users.noreply.github.com>
2024-06-07 16:13:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud_city():
-    return render_template('index.html')
+    return "Hello, Welcome to Cloud City!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
