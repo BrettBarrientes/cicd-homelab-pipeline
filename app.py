d3a2dd7f6d63189356fb758c9eebdb2b1ac40827
Brett Barrientes <51255310+BrettBarrientes@users.noreply.github.com>
2024-06-07 15:52:14 -0400

    Update app.py
    Update function

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_cloud_city():
     return 'Hello, Welcome to Cloud City!!'
 
 if __name__ == '__main__':
